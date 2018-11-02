87c0bd25c800a8c67ea62703f02de7fced85142b
Landy Chan <Landychan92@gmail.com>
2018-11-01 20:45:42 -0700

    Moved subreddit view to a fragment

---
+++ app/src/main/java/chan/landy/redditclient/ClientUtils.java
@@ -0,0 +1,17 @@
+package chan.landy.redditclient;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class ClientUtils {
+
+}
--- app/src/main/java/chan/landy/redditclient/WebViewActivity.java
+++ app/src/main/java/chan/landy/redditclient/WebViewActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.util.Log;
 import android.webkit.WebView;
 
@@ -15,6 +16,7 @@ public class WebViewActivity extends Activity {
 
     final String TAG = "WebViewActivity";
 
+    @BindView(R.id.webview_progressbar) ContentLoadingProgressBar progressBar;
     @BindView(R.id.activity_webview) WebView webView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
